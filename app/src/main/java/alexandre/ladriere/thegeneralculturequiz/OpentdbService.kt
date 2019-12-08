c39b2624739d0986a1c574919b06e742a6dcf35b
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2019-12-08 13:53:13 +0100

    add: Init project and adding QuestionModel

---
+++ app/src/main/java/alexandre/ladriere/thegeneralculturequiz/OpentdbService.kt
@@ -0,0 +1,28 @@
+package alexandre.ladriere.thegeneralculturequiz
+
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpentdbService {
+    @GET("api.php")
+    fun getQuestions(@Query("amount") amount: String = "10",
+                      @Query("category") category: String = "",
+                      @Query("difficulty") difficulty: String = "",
+                      @Query("type") type: String = "multiple"):
+            Observable<QuestionModel.Response>
+
+    companion object {
+        fun create(): OpentdbService {
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://opentdb.com/")
+                .build()
+            return retrofit.create(OpentdbService::class.java)
+        }
+    }
+}
