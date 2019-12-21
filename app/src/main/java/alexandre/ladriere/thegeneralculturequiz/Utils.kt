1f82a0ac9131f37b6694f6ba5cc2ac5932a517be
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2019-12-21 16:24:09 +0100

    add: Adding Next Button in QuestionActivity to go to next question

--- app/src/main/java/alexandre/ladriere/thegeneralculturequiz/Utils.kt
+++ app/src/main/java/alexandre/ladriere/thegeneralculturequiz/Utils.kt
@@ -4,5 +4,9 @@ internal fun removeSpecialCharFromString(string: String): String {
     var outStr = string.replace("&#039;", "'")
     outStr = outStr.replace("&quot;", "\"")
     outStr = outStr.replace("&ouml;", "ö")
+    outStr = outStr.replace("&deg;", "°")
+    outStr = outStr.replace("&ldquo;", "“")
+    outStr = outStr.replace("&rdquo;", "”")
+    // TODO add &shy;
     return outStr
 }
